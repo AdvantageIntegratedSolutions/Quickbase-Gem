faefb17a431fa447210923738aeab86c24845888
Ben Roux <liquid.ise@gmail.com>
2014-03-31 17:38:09 -0600

    We can officially connect to quickbase

--- quickbase.rb
+++ quickbase.rb
@@ -1,3 +1,4 @@
+require 'net/https'
 require 'nokogiri'
 require 'json'
 
@@ -13,21 +14,35 @@ module Advantage
                       "<apptoken>#{app_token}</apptoken>" +
                     "</qdbapi>"
 
-      puts build_request_url( :authenticate )
-      puts build_request_headers( :authenticate, request_xml )
+      url = build_request_url( :authenticate )
+      headers = build_request_headers( :authenticate, request_xml )
+
+      @http = Net::HTTP.new( base_url, 443 )
+      @http.use_ssl = true
+      @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      result = @http.post( url, request_xml, headers )
+      parsed_result = Nokogiri::HTML( result.body )
+
+      ticket = parsed_result.css( 'ticket' )
+      if !ticket.empty?
+        @ticket = ticket.text
+      else
+        raise "Connection Failed\n\n#{result.body}"
+      end
     end
 
     private
     def build_request_headers( api_call, request_body )
       {
         'Content-Type'      => 'application/xml',
-        'Content-Length'    => request_body.length,
-        'QUICKBASE-ACTION'  => api_call
+        'Content-Length'    => request_body.length.to_s,
+        'QUICKBASE-ACTION'  => "API_#{api_call}"
       }
     end
 
     def build_request_url( api_call )
-      url = "http://#{@domain}.quickbase.com"
+      url = base_url
 
       # Different calls have different API paths. Assign those here
       case api_call
@@ -35,6 +50,10 @@ module Advantage
         url += '/db/main'
       else
       end
+    end
+
+    def base_url
+      "https://#{@domain}.quickbase.com"
     end
   end
 end
